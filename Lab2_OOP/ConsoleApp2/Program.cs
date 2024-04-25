d277f13cd55ba0851c1ff3ca1151b6a7565a48cd
Animouss <145475094+Animouss@users.noreply.github.com>
2024-04-25 20:32:13 +0300

    Add files via upload

---
+++ Lab2_OOP/ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+
+        using var httpClient = new HttpClient();
+
+
+        string[] urls = { "https://jsonplaceholder.typicode.com/posts/1", "https://jsonplaceholder.typicode.com/posts/2", "https://jsonplaceholder.typicode.com/posts/3" };
+
+      
+        TimeSpan totalTime = TimeSpan.Zero;
+
+        
+        foreach (var url in urls)
+        {
+            try
+            {
+                
+                var stopwatch = Stopwatch.StartNew();
+               
+                var response = await httpClient.GetAsync(url);               
+                stopwatch.Stop();
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                
+                Console.WriteLine($"Response from {url}: {responseBody}");
+                totalTime += stopwatch.Elapsed;
+            }
+            catch (HttpRequestException ex)
+            {
+                
+                Console.WriteLine($"Error  data from {url}: {ex.Message}");
+            }
+        }
+        Console.WriteLine($"Total  time: {totalTime}");
+    }
+}
---
+++ Lab2_OOP/CyncMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        string[] urls = { "https://jsonplaceholder.typicode.com/posts/1", "https://jsonplaceholder.typicode.com/posts/2", "https://jsonplaceholder.typicode.com/posts/3" };
+
+        HttpClient client = new HttpClient();
+        foreach (string url in urls)
+        {
+            try
+            {
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                response.EnsureSuccessStatusCode();
+                string responseBody = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Request to {url} failed with message: {e.Message}");
+            }
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Total  time: {stopwatch.ElapsedMilliseconds} ms");
+    }
+}
